d455c4cd19db878ebc382c222f35d2cbe8d62038
TopherCalvin <126538023+TopherCalvin@users.noreply.github.com>
2023-04-08 14:46:04 +0700

    up

--- Day 07/spotify(me)/src/App.js
+++ Day 07/spotify(me)/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/" element={<HomePage}></Route>
     </Routes>
   );
 }
